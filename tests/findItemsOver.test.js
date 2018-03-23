0e4fbd541444d7e53eecd39692e981981afaf5a0
coder <mshawnlouw@gmail.com>
2018-03-23 10:22:00 +0200

    rectified

--- tests/findItemsOver.test.js
+++ tests/findItemsOver.test.js
@@ -1,6 +1,6 @@
 describe('The findItemsOver function', function(){
   it('it should return items/products that are higher in Quantity than threshold', function(){
-    assert.deepEqual(findItemsOver([{name:"apples", "qty":40} , {name: "bananas", qty: 23},],20), [{"name":"apples", "qty": 40}, {"name":"bananas","qty":23},], true)
+    assert.deepEqual(findItemsOver([{name:"apples", "qty":40} , {name: "bananas", qty: 23},], [{"name":"apples", "qty": 40}, {"name":"bananas","qty":23},], true)
   });
   it('it should return items/products that are lower in Quantity than threshold', function(){
     assert.deepEqual(findItemsOver([{name:"apples", "qty":15} , {name: "bananas", qty:19 },],20), [])
