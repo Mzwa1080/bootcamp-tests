1e1e174163d586d1a7e19b913e0ebe345a5c06c4
coder <mshawnlouw@gmail.com>
2018-03-27 14:37:22 +0200

    Final commit

--- tests/findItemsOver20.test.js
+++ tests/findItemsOver20.test.js
@@ -3,6 +3,6 @@ describe("The findItemsOver20 function", function(){
     assert.deepEqual(findItemsOver20([{name:"apples", qty:10}, {name:"pears",qty:37}, {name:"bananas",qty:27}, ]), [{"name": "pears","qty": 37}, {"name":"bananas","qty":27}, ])
   });
   it("should return no items with quantiy over 20", function(){
-    assert.notEqual(findItemsOver20([{name:"apples", qty:10}, {name:"pears",qty:15}, {name:"bananas",qty:15}, ], true));
+    assert.notEqual(findItemsOver20([''],'', true));
 });
 });
