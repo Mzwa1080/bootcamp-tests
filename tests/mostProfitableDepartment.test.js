bb60ed532d6ddf4bc40891d656480b3a573009f1
coder <mshawnlouw@gmail.com>
2018-03-27 14:25:52 +0200

    Final commit

--- tests/mostProfitableDepartment.test.js
+++ tests/mostProfitableDepartment.test.js
@@ -20,7 +20,7 @@ describe('The mostProfitableDepartment function', function(){
       assert.notEqual(mostProfitableDepartment(salesData), true);
   });
   it("If the function fails determine which  department is the most profitable",function(){
-  assert.equal(mostProfitableDepartment([{department : 'hardware', sales : 4500},{department : 'outdoor', sales : 1500}]), 'hardware')
+  assert.equal(mostProfitableDepartment(''), ' ')
 });
 
 });
